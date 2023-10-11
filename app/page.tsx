44a60e0b6c0e6627fdf1a9eb44b2471b84cfc476
Thabish <kadertabish2@gmail.com>
2023-10-11 06:01:32 +0530

    titile change
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,14 @@ export default function Home() {
 		<div className="p-12">
 			<h1 className="font-bold text-4xl ">Kinde Auth.</h1>
 			<div className="flex flex-col h-[500px] items-center justify-center">
-				<h1 className="font-bold text-4xl">Home Page.</h1>
+				<h1 className="font-bold text-6xl">
+					Simple, powerful authentication
+				</h1>
 				<div className="flex items-center space-x-2 mt-12">
-					<LoginLink className="bg-black text-white px-2 py-1 rounded-md">
+					<LoginLink className="bg-black text-white px-4 text-xl py-2 rounded-md">
 						Sign In
 					</LoginLink>
-					<RegisterLink className="border px-2 py-1 rounded-md">
+					<RegisterLink className="border px-4 text-xl py-2 rounded-md">
 						Sign up
 					</RegisterLink>
 				</div>
